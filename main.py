6d61b7aecc46bcefc6b8d6ef302ee73db11200f4
José A. Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2023-05-26 18:06:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scraper import encontre_noticias
 
 def main():
-    encontre_noticias(busca="Agressão",
-                      inicio="22-08-2022",
-                      fim="07-01-2023")
+    encontre_noticias(busca="",
+                      inicio="",
+                      fim="")
 
 if __name__ == "__main__":
     main()
