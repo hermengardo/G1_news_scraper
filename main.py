bdff0e3b244dd37ae5462d79246cbac36802ddfd
a <josealexandre>
2024-05-13 18:41:41 -0300

    max results parameter added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from scraper import encontre_noticias
 
 def main():
-    encontre_noticias(busca="",
-                      inicio="",
-                      fim="")
+    encontre_noticias(busca="bolsonaro",
+                      inicio="01-01-2018",
+                      fim="10-11-2018",
+                      max_results=10)
 
 if __name__ == "__main__":
     main()
--- scraper.py
+++ scraper.py
@@ -8,9 +8,9 @@ from lxml import html
 import utils
 
 
-global BASE_URL, HEADER
+global BASE_URL, HEADERS
 BASE_URL = "https://g1.globo.com/busca/"
-HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
           "Accept": "*/*",
           "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6",
           "Accept-Encoding": "gzip, deflate, br"}
@@ -23,55 +23,75 @@ class encontre_noticias():
                  fim,
                  delay=0.1,
                  filepath="data.csv",
-                 retry=3,
-                 timeout=30):
+                 max_retries=3,
+                 timeout=30,
+                 max_results=None):
+        self.busca = busca
         since = utils.convert_to_datetime(inicio)
         until = utils.convert_to_datetime(fim)
-        self.busca = busca
-        self.base_query = {"q": busca,
-                           "page": 1,
-                           "order": "recent",
-                           "from": since,
-                           "to": until,
-                           "species":"notícias",
-                           "ajax": "1"}
-        self.filepath = filepath
-        self.delay = delay
-        self.count = 0
-        self.retry = retry
-        self.retry_count = 0
-        self.timeout = timeout
-        utils.clean_file(self.filepath)
-        self.pipeline()
+        
+        self.params = {
+            "q": busca,
+            "page": 1,
+            "order": "recent",
+            "from": since,
+            "to": until,
+            "species":"notícias",
+            "ajax": "1"
+        }
 
-    def pipeline(self) -> None:
+        self.filepath = filepath
+        utils.clean_file(self.filepath)
+
+        self.delay = delay
+        self.max_retries = max_retries
+        self.retry_reset = max_retries
+        self.timeout = timeout
+        self.max_results = max_results
+        self.max_reached = False
+        self.count = 0
+        
+        self.run()
+
+    def run(self) -> None:
         while True:
             try:
-                tree = self.load_feed()
+                tree = self.fetch(BASE_URL)
                 if self.check_end_page(tree):
                     print('Fim da coleta.')
                     break
                 articles = self.find_urls(tree)
                 for article in articles:
+                    if self.max_results != None and self.count >= self.max_results:
+                        print('Número máximo de resultados atingido.')
+                        self.max_reached = True
+                        break
                     self.scrape_content(article)
                     sleep(self.delay)
-                self.base_query['page'] += 1
-                self.retry_count = 0  # Reset the retry count if the request is successful
+                if self.max_reached:
+                    print('Fim da coleta.')
+                    break
+                self.params['page'] += 1
+                self.max_retries = self.retry_reset  # Reset the retry count if the request is successful
             except requests.exceptions.RequestException:
-                self.retry_count += 1
-                if self.retry_count > self.retry:
+                self.max_retries -= 1
+                if self.max_retries == 0:
                     print("Número máximo de tentativas excedido. Fechando conexão...")
                     break
                 print(f"Perda de conexão... Nova tentativa em {self.timeout} segundos")
                 sleep(self.timeout)
                 continue
 
-    def load_feed(self) -> html.HtmlElement:
-        response = requests.request("GET", BASE_URL, params=self.base_query, headers=HEADER)
-        return utils.html_parser(response)
-
-    def load_page(self, url: str) -> html.HtmlElement:
-        response = requests.request("GET", url, headers=HEADER)
+    def fetch(self, url) -> html.HtmlElement:
+        response = (
+            requests
+            .request(
+                "GET", 
+                url, 
+                params=self.params, 
+                headers=HEADERS
+            )
+        )
         return utils.html_parser(response)
 
     def find_urls(self, tree: html.HtmlElement) -> list:
@@ -80,13 +100,10 @@ class encontre_noticias():
 
     def scrape_content(self, article) -> None:
         data = {}
-        # Carrega a página
-        article_tree = self.load_page(article)
+        article_tree = self.fetch(article)
         if self.article_is_valid(article_tree):
-            # Extrai dados (campos não fixos)
             data['subtitulo'] = self.get_data(article_tree, 'h2[class=content-head__subtitle]')
             data['autor'] = self.get_data(article_tree, 'p[class=content-publication-data__from]')
-            # Extrai dados (campos fixos)
             data['data'] = self.get_data(article_tree, 'time[itemprop=datePublished]', 'datetime')
             data['titulo'] = self.get_data(article_tree, 'div[class=title] > meta[itemprop=name]', 'content')
             data['conteudo'] = self.get_media_content(article_tree, 'div.mc-column.content-text.active-extra-styles')
@@ -95,7 +112,6 @@ class encontre_noticias():
             data['busca'] = self.busca
             self.count += 1
             print(f'Coletado: {self.count} | {data["titulo"][:30]}... | {data["autor"]}')
-            # Salva os dados
             utils.save_data_to_csv(data=data, filepath=self.filepath)
         else:
             return None
